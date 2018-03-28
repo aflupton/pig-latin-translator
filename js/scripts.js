b501294c1335c94e4ec7859a7ac98a1243c21047
andrew lupton and Alex Noble <alex@>
2018-03-28 10:31:16 -0700

    add back-end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,34 @@
-// business logic
+var firstWord="";
+var vowels=["a","e","i","o","u"];
 
+// business logic
+var pigLatin = function(sentence) {
+
+}
 
 // user interface logic
 $(document).ready(function() {
   $("form#translator").submit(function(event) {
     event.preventDefault();
     var sentence = ($("input#latin").val());
-
-
-
-
-    $(".output").text("");
+    var index=sentence.indexOf(" ");
+    firstWord = sentence.slice(0);
+    if (firstWord.length=="1"){
+      vowels.forEach(function(vowel){
+        if(firstWord.charAt(0)===vowel.toString()){
+          firstWord=firstWord+"ay";
+        } else{}
+      });
+    } else {
+      vowels.forEach(function(vowel){
+        if(firstWord.charAt(0)===vowel.toString()){
+          firstWord=firstWord+"way";
+        } else{};
+      });
     }
+
+    $(".output").text(firstWord);
+
 
     $("#result").show();
   });
