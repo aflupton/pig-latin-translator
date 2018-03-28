e53fc27c3dbcf19008d3749b09cf477710564ea6
andrew lupton and Alex Noble <alex@>
2018-03-28 14:46:40 -0700

    add multi-word functionality

--- js/scripts.js
+++ js/scripts.js
@@ -6,67 +6,77 @@ var tempConsonant="";
 var finalWord="";
 var firstVowel=0;
 var consonant;
+var allWords=[];
+var finalWords=[];
 
 $(document).ready(function() {
   $("form#translator").submit(function(event) {
     event.preventDefault();
-    var sentence = ($("input#latin").val());
-    //var index=sentence.indexOf(" ");
-    firstWord = sentence.slice(0);
-    if (firstWord.length=="1"){
-      vowels.forEach(function(vowel){
-        if(firstWord.charAt(0)===vowel.toString()){
-          firstWord=firstWord+"ay";
-        }
-        else{}
-      });
-    }
-    else {
-      vowels.forEach(function(vowel) {
-        if(firstWord.charAt(0)===vowel.toString()){
-          firstWord=firstWord+"way";
-          firstVowel++;
-        }
-        else{}
-      });
-      if(firstVowel==0){
-        //consonants.forEach(function(consonant){
+    var sentence = $("input#latin").val();
+    allWords=sentence.split(" ");
 
-        for(z=0;z<consonants.length;z++){
-          consonant=consonants[z];
-          for(x=0; x<firstWord.length; x++){
-            if(consonant===firstWord.charAt(x)&&consonant==="q"){
-              if(firstWord.charAt(x+1)==="u"){
-                tempConsonant=tempConsonant+"qu";
-                x++;
-                z=0;
+    //var index=sentence.indexOf(" ");
+    allWords.forEach(function(word){
+      firstWord = word;
+      if (firstWord.length=="1"){
+        vowels.forEach(function(vowel){
+          if(firstWord.charAt(0)===vowel.toString()){
+            firstWord=firstWord+"ay";
+          }
+          else{}
+        });
+      }
+      else {
+        vowels.forEach(function(vowel) {
+          if(firstWord.charAt(0)===vowel.toString()){
+            firstWord=firstWord+"way";
+            firstVowel++;
+          }
+          else{}
+        });
+        if(firstVowel==0){
+          //consonants.forEach(function(consonant){
+
+          for(z=0;z<consonants.length;z++){
+            consonant=consonants[z];
+            for(x=0; x<firstWord.length; x++){
+              if(consonant===firstWord.charAt(x)&&consonant==="q"){
+                if(firstWord.charAt(x+1)==="u"){
+                  tempConsonant=tempConsonant+"qu";
+                  x++;
+                  z=0;
+                }
+                else{
+                  tempConsonant=tempConsonant+consonant;
+                  z=0;
+                }
               }
               else{
-                tempConsonant=tempConsonant+consonant;
-                z=0;
+                if(firstWord.charAt(x)===consonant.toString()){
+                  tempConsonant=tempConsonant+consonant;
+                  z=0;
+                  //firstWord=firstWord.slice(x);
+                }
+                else{
+                  firstWord=firstWord.slice(x);
+                  x=firstWord.length +1;
+                }
               }
             }
-            else{
-              if(firstWord.charAt(x)===consonant.toString()){
-                tempConsonant=tempConsonant+consonant;
-                z=0;
-                //firstWord=firstWord.slice(x);
-              }
-              else{
-                firstWord=firstWord.slice(x);
-                x=firstWord.length +1;
-              }
-            }
+
           }
 
-        }
+        //  });
+          firstWord=firstWord+tempConsonant+"ay";
+          tempConsonant="";
+        } else{};
+      }
+      finalWords.push(firstWord);
+    });
 
-      //  });
-        firstWord=firstWord+tempConsonant+"ay";
-        tempConsonant="";
-      } else{};
-    }
-    $(".output").text(firstWord);
+    //finalWords.reverse();
+
+    $(".output").text(finalWords.join(" "));
     $("#result").show();
   });
 });
